bcb43e7f245a1a86d230908140b481a69c20e6a0
Kathi Einzenberger <katharinaeinzi@gmail.com>
2024-06-11 09:49:18 +0200

    fix sign up

--- login/signUp.js
+++ login/signUp.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const email = document.getElementsByClassName('email-input')[0].value;
         const username = document.getElementsByClassName('username-input')[0].value;
         const birthday = document.getElementsByClassName('birthday-input')[0].value;
-        // const password = document.getElementsByClassName('password-input')[0].value;
+        const password = document.getElementsByClassName('password-input')[0].value;
 
         const response = await fetch('http://localhost:3000/users', {
             method: 'POST',
