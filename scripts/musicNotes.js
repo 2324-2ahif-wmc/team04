ae94b20deea029ce9350d87870344a9d36f513ea
Flora Dallinger <114140834+Floraxoxo@users.noreply.github.com>
2024-06-24 17:24:18 +0200

    adding Library

--- scripts/musicNotes.js
+++ scripts/musicNotes.js
@@ -1,16 +1,25 @@
 const chartToMusic = document.getElementById('js-ChartToMusicSwitchButton');
 const musicToChart = document.getElementById('js-MusicToChartSwitchButton');
 
+const div = document.getElementById("js-sheetNoteMusic");
+
 const diagram = document.getElementById('js-chart');
 
 chartToMusic.addEventListener('click', () =>{
+    div.classList.remove('hidden');
     diagram.classList.add('hidden');
     chartToMusic.classList.add('hidden');
     musicToChart.classList.remove('hidden');
+    drawBackground();
 });
 
 musicToChart.addEventListener('click', () =>{
+    div.classList.add('hidden');
     diagram.classList.remove('hidden');
     chartToMusic.classList.remove('hidden');
     musicToChart.classList.add('hidden');
 });
+
+function drawBackground(){
+    
+}
